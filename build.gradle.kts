26aee8285b1390cd322556598313466122355834
Ali Lozano <alilozanoc@gmail.com>
2024-04-18 22:44:48 -0300

    ci: sonarqube added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,12 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
+
+    id("org.sonarqube") version "4.0.0.2929"
+    id("jacoco")
+    id("checkstyle")
 }
+
 
 group = "pe.edu.upao.alozanoc"
 version = "0.0.1-SNAPSHOT"
@@ -32,4 +37,41 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+
+sonar {
+    properties {
+        property("sonar.projectName", "arquitecturaapp")
+        property("sonar.projectKey", "arquitecturaapp")
+        property("sonar.host.url", "http://localhost:9001")
+        property("sonar.login", "sqa_757d7b36f0c1a229ebba160a88cf1a74ef094648");
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+    reportsDirectory = rootProject.file("reports/jacoco/jacocoRootReport")
+}
+
+tasks.withType<Pmd> {
+    reports {
+        xml.required = true
+        html.required = true
+    }
+}
+
+tasks.withType<Checkstyle> {
+    reports {
+        this.html.outputLocation = rootProject.file("reports/checkstyle/checkstyle.html")
+    }
+}
+
+
+tasks.named("sonar") {
+    dependsOn("jacocoTestReport")
+}
+
+tasks.named("jacocoTestReport") {
+    dependsOn("test")
 }
